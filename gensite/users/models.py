d289383eaa43acea3a160f564137904dcaed2c5e
diwasblack <diwasblack@gmail.com>
2014-06-04 17:55:24 +0545

    Web Server for Gen

---
+++ gensite/users/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class user(models.Model):	
+	first_name= models.CharField(max_length=50)
+	last_name=models.CharField(max_length=50)
+	email=models.CharField(max_length=50)
+	username = models.CharField(max_length=50)
+	password= models.TextField()
+
+
