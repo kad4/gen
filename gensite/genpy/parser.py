f1e880f3c99a5f4a73d163e1085f6b00d08e21cc
unknown <subedi.aayush@gmail.com>
2014-07-27 16:05:25 +0545

    Built parser to parse RSS Feeds

--- gensite/genpy/parser.py
+++ gensite/genpy/parser.py
@@ -4,55 +4,66 @@ import re
 import sys
 import threading
 import urllib.request
+import time
 
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 from urllib.request import urlopen
 
+
 def parser(URL):
-	print ("Parsing:",URL)
-	try:
-		currentPage = urlopen(URL).read()
-		soup = BeautifulSoup(currentPage)
-		# print (currentPage)
-		if soup.rss == None:
-			parseHTML(URL, currentPage, soup)
-		else:
-			parseXML(URL, currentPage, soup)
-	except Exception as e:
-		print ('Error parsing',URL)
-		print (e)
+    print("Parsing:", URL)
+    try:
+        currentPage = urlopen(URL).read()
+        soup = BeautifulSoup(currentPage)
+        if soup.rss is None:
+            return parseHTML(URL, currentPage, soup)
+        else:
+            return parseXML(URL, currentPage, soup)
+    except Exception as e:
+        print('Error parsing', URL)
+        print(e)
+
 
 def parseHTML(URL, PAGE, SOUP):
-	print ("Parsing HTML")
-	try:
-		currentPage = PAGE
-		soup = SOUP
+    print("Parsing HTML")
+    try:
+        currentPage = PAGE
+        soup = SOUP
 
-		titles = []
-		
-		for link in soup.find_all('a'):
-			titles.append([link.string, link.get('href')])
-			
-		f = open('list.lst', 'wb')
-		print ("written")
-		pickle.dump(titles,f, protocol = pickle.HIGHEST_PROTOCOL)
+        titles = []
+
+        for link in soup.find_all('a'):
+            titles.append([link.string, link.get('href')])
+
+        f = open('list.lst', 'wb')
+        print("written")
+        pickle.dump(titles, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 
-	except Exception as e:
-		print (e)
-		raise
-		
 def parseXML(URL, PAGE, SOUP):
-	print ("Parsing XML")
-	try:
-		currentPage = urlopen(URL).read()
-		soup = BeautifulSoup(currentPage, 'xml')
+    print("Parsing XML")
+    pageOutput = []
+    try:
+        currentPage = PAGE
+        soup = BeautifulSoup(currentPage, 'xml')
 
+        itemList = soup.findAll('item')
+        for item in itemList:
+            articleTitle = item.title.text.encode('utf-8')
+            articleDate = list(time.strptime(item.pubDate.text, '%a, %d %b %Y %X %z')[:3])  # Thu, 24 Jul 2014 11:32:24 +0000
+            articleURL = item.link.text
+            pageOutput.append([articleTitle, articleDate, articleURL])
 
-	except Exception as e:
-		print (e)
-		raise
-		
+        return pageOutput
+    except Exception as e:
+        print(e)
+        return False
+
 if __name__ == '__main__':
-	parser('http://www.ekantipur.com/np/archive/')
+    print(parser('http://localhost/gen/feed.xml'))
