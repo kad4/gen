fa0b3715f12df9ac86017db3860c6c94d3f76ffb
diwasblack <diwasblack@gmail.com>
2014-08-04 22:45:06 +0545

    Alterations for rssreader

--- gensite/rssreader/tasks.py
+++ gensite/rssreader/tasks.py
@@ -1,15 +1,10 @@
 from __future__ import absolute_import
 
 from celery import shared_task
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task
 
-@shared_task
-def add(x, y):
-    return x + y
-
-@shared_task
-def mul(x, y):
-    return x * y
-
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
+@periodic_task(run_every=(crontab(hour="*", minute="*",day_of_week="*")))
+def scraper_example():
+	logger.info("Running")
+	
