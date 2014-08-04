f78ce9917ed8a11b89402e2340cc4f98e4b472eb
diwasblack <diwasblack@gmail.com>
2014-08-04 23:01:10 +0545

    Removed task file used for testing

--- gensite/rssreader/tasks.py
+++ gensite/rssreader/tasks.py
@@ -5,6 +5,6 @@ from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 
 @periodic_task(run_every=(crontab(hour="*", minute="*",day_of_week="*")))
-def scraper_example():
+def rssreader():
 	logger.info("Running")
 	
