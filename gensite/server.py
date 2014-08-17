7eb65e8f9fa9687480638b918774d375806ff53d
diwasblack <diwasblack@gmail.com>
2014-08-17 14:39:38 +0545

    Socket files

---
+++ gensite/server.py
@@ -0,0 +1,26 @@
+# This is server.py file
+import socket 
+import pickle
+from sys import getsizeof
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+port = 6200
+s.bind(('127.0.0.1', port))
+s.listen(5)
+
+# while True:
+c, addr = s.accept()
+request=pickle.loads(c.recv(1024))
+
+# pickle_data=None
+pickle_data=pickle.dumps('hail hydra')
+c.send(pickle.dumps(getsizeof(pickle_data)))
+
+if(request['action']=='login'):
+	print('received')
+	c.send(pickle_data)
+
+elif(request['action']=='login'):
+	pass
+
+c.close()
